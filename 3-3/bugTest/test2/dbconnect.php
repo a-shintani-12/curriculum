9ff6a0b4e6e53b611337f43c6c97e173cb620f6f
新谷厚弥 <a-shintani@y-i-group.co.jp>
2020-11-26 19:26:25 +0900

    バグ修正を行う

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+// セッション開始
+// ▼▼ 削除 ▼▼ session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+// ▼▼ rootを追加 ▼▼
+$db['pass'] = "root";
+// データベース名
+// ▼▼ yigroupBlogに修正 ▼▼
+$db['dbname'] = "yigroupBlog2";
